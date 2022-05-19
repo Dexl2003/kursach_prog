8f07c2267ea2c2835f6d42cdf291b121a7e4c62d
Dexl2000 <dexl386@gmail.com>
2022-05-20 01:48:40 +0800

    DB_Class_contruction

---
+++ kursach_prog/DataBaseConnector.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+
+namespace kursach_prog
+{
+    internal class DataBaseConnector
+    {
+        public static string connectString;
+
+        static string host = "localhost";
+
+        static string login = "root";
+        static string password = "root";
+
+        static string DB_Name = "cpjkurs";
+        static MySqlConnection conn = new MySqlConnection(connectString);
+
+
+        public static void OpenConnection() 
+        {
+            try
+            {
+                connectString = "server=localhost;port=3306;database=cpjkurs;user=root;password=root;" /*+ host + ";Database=" + DB_Name + ";Uid=" + login + ";pwd=" + password + ";"*/;
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        public static List<string[]> ExecuteQuery (int columnCount)
+        {
+            try
+            {
+                string query = "SELECT * FROM catalog"; // set query to fetch data "Select * from  tabelname"; 
+                MySqlCommand command = new MySqlCommand(query, conn);
+
+                MySqlDataReader reader = command.ExecuteReader();  
+
+                MessageBox.Show("Error reader");
+
+                List<string[]> resoult = new List<string[]>();
+
+                while (reader.Read())
+                {
+                    resoult.Add(new string[columnCount]);
+
+                    for (int i = 0; i < columnCount; i++)
+                    {
+                        resoult[resoult.Count - 1][i] = reader[i].ToString();
+                    }
+                    reader.Close();
+                }
+                MessageBox.Show("Error while");
+                if (resoult.Count != 0)
+                {
+                    return resoult;
+                    MessageBox.Show("Error return");
+                }
+                else
+                {
+                    return null;
+                    MessageBox.Show("Error return");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
+
+
+        public static void CloseConnection()
+        {
+            conn.Close();
+        }
+
+
+
+
+
+    }
+}
--- kursach_prog/Form1.Designer.cs
+++ kursach_prog/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.top = new System.Windows.Forms.RadioButton();
             this.sale = new System.Windows.Forms.RadioButton();
             this.from = new System.Windows.Forms.TextBox();
@@ -37,22 +36,14 @@
             this.label2 = new System.Windows.Forms.Label();
             this.errorText = new System.Windows.Forms.Label();
             this.fullList = new System.Windows.Forms.RadioButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(12, 12);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(199, 424);
-            this.listBox1.TabIndex = 0;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // top
             // 
             this.top.AutoSize = true;
-            this.top.Location = new System.Drawing.Point(229, 50);
+            this.top.Location = new System.Drawing.Point(12, 48);
             this.top.Name = "top";
             this.top.Size = new System.Drawing.Size(60, 19);
             this.top.TabIndex = 1;
@@ -63,7 +54,7 @@
             // sale
             // 
             this.sale.AutoSize = true;
-            this.sale.Location = new System.Drawing.Point(229, 88);
+            this.sale.Location = new System.Drawing.Point(12, 86);
             this.sale.Name = "sale";
             this.sale.Size = new System.Drawing.Size(137, 19);
             this.sale.TabIndex = 2;
@@ -73,7 +64,7 @@
             // 
             // from
             // 
-            this.from.Location = new System.Drawing.Point(229, 139);
+            this.from.Location = new System.Drawing.Point(12, 137);
             this.from.Name = "from";
             this.from.Size = new System.Drawing.Size(60, 23);
             this.from.TabIndex = 3;
@@ -81,7 +72,7 @@
             // 
             // before
             // 
-            this.before.Location = new System.Drawing.Point(306, 139);
+            this.before.Location = new System.Drawing.Point(89, 137);
             this.before.Name = "before";
             this.before.Size = new System.Drawing.Size(60, 23);
             this.before.TabIndex = 4;
@@ -90,7 +81,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(249, 121);
+            this.label1.Location = new System.Drawing.Point(32, 119);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(21, 15);
             this.label1.TabIndex = 5;
@@ -99,7 +90,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(324, 121);
+            this.label2.Location = new System.Drawing.Point(107, 119);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(22, 15);
             this.label2.TabIndex = 6;
@@ -108,7 +99,7 @@
             // errorText
             // 
             this.errorText.AutoSize = true;
-            this.errorText.Location = new System.Drawing.Point(229, 179);
+            this.errorText.Location = new System.Drawing.Point(12, 177);
             this.errorText.Name = "errorText";
             this.errorText.Size = new System.Drawing.Size(0, 15);
             this.errorText.TabIndex = 8;
@@ -116,21 +107,30 @@
             // fullList
             // 
             this.fullList.AutoSize = true;
-            this.fullList.Checked = true;
-            this.fullList.Location = new System.Drawing.Point(229, 12);
+            this.fullList.Location = new System.Drawing.Point(12, 10);
             this.fullList.Name = "fullList";
             this.fullList.Size = new System.Drawing.Size(95, 19);
             this.fullList.TabIndex = 9;
-            this.fullList.TabStop = true;
             this.fullList.Text = "Весь каталог";
             this.fullList.UseVisualStyleBackColor = true;
             this.fullList.CheckedChanged += new System.EventHandler(this.fullList_CheckedChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(180, 10);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(478, 443);
+            this.dataGridView1.TabIndex = 11;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(450, 450);
+            this.ClientSize = new System.Drawing.Size(667, 465);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.fullList);
             this.Controls.Add(this.errorText);
             this.Controls.Add(this.label2);
@@ -139,18 +139,16 @@
             this.Controls.Add(this.from);
             this.Controls.Add(this.sale);
             this.Controls.Add(this.top);
-            this.Controls.Add(this.listBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private ListBox listBox1;
         private RadioButton top;
         private RadioButton sale;
         private TextBox from;
@@ -159,5 +157,6 @@
         private Label label2;
         private Label errorText;
         private RadioButton fullList;
+        private DataGridView dataGridView1;
     }
 }
